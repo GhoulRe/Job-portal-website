066285c6cc58414292658518c5ec9b419c7cd7e5
GhoulRe <77874501+GhoulRe@users.noreply.github.com>
2024-04-30 00:29:50 +0000

    store application in database

--- database.py
+++ database.py
@@ -24,3 +24,37 @@ def load_work_from_db(id):
       return row._asdict()
     else:
       return None
+
+
+# def add_application_to_db(job_id, data):
+#   with engine.connect() as conn:
+#     query = text(
+#       "INSERT INTO applications (job_id, full_name, email, linkedin_url, education, experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :experience, :resume_url)"
+#     )
+#     conn.execute(query,
+#              job_id=job_id,
+#              full_name=data['full_name'],
+#              email=data['email'],
+#              # linkedin_url=data['linkedin_url'],
+#              education=data['education'],
+#              work_experience=data['experience'],
+#              resume_url=data['resume_url'])
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+      query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+
+      params = {
+          'job_id': job_id,
+          'full_name': data['full_name'],
+          'email': data['email'],
+          'linkedin_url': data['linkedin_url'],
+          'education': data['education'],
+          'work_experience': data['work_experience'],
+          'resume_url': data['resume_url']
+      }
+
+      result = conn.execute(query, params)
+      # Commit the transaction
+      conn.commit()
+      return result
